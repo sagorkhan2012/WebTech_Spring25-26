30b16d8e8a92cd22620f58058bfaceefcc075bef
sagorkhan2012 <sagor2012.sk@gmail.com>
2026-03-08 17:59:51 +0600

    update lab practice performance

---
+++ Lab Practice Performance /js/script.js
@@ -0,0 +1,28 @@
+let form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let fname = document.getElementById("fname").value;
+  let lname = document.getElementById("lname").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let message = document.getElementById("message").value;
+
+  if (
+    fname == "" ||
+    lname == "" ||
+    email == "" ||
+    phone == "" ||
+    message == ""
+  ) {
+    alert("Field Value need to be filled up");
+    return;
+  }
+
+  console.log("First Name:", fname);
+  console.log("Last Name:", lname);
+  console.log("Email:", email);
+  console.log("Phone:", phone);
+  console.log("Message:", message);
+});
